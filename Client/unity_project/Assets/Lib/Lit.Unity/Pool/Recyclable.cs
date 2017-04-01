aec97fc4dcf13b1e1a6c475dbf9e99ca7614a244
jiuguang <780632213@qq.com>
2017-04-01 09:50:45 +0800

    增加普通类缓存管理

---
+++ Client/unity_project/Assets/Lib/Lit.Unity/Pool/Recyclable.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lit.Unity
+{
+    public  abstract class Recyclable<T> where T : Recyclable<T>, new()
+    {
+        private static LinkedList<T> pool = new LinkedList<T>();
+
+        private bool _active = false;
+        /// <summary>
+        /// 此处Active表示是否已经被GC
+        /// </summary>
+        public bool Active { get { return _active; } }
+
+        public virtual void GC()
+        {
+            if (_active)
+            {
+                pool.AddLast(this as T);
+                this.OnDisable();
+                _active = false;
+            }else
+            {
+                LitLogger.ErrorFormat("Obj has been GC <{0}>", this);
+            }
+        }
+
+        public static void ClearAll()
+        {
+            pool.Clear();
+        }
+
+
+        public static T New()
+        {
+            T t = null;
+            if (pool.Count > 0)
+            {
+                t = pool.First as T;
+                pool.RemoveFirst();
+            }
+            if (t == null)
+            {
+                t = new T();
+                t.OnCreate();
+            }
+            t._active= true;
+            t.OnEnable();
+            return t;
+        }
+
+
+        /// <summary>
+        /// G:初次被创建的时候调用
+        /// </summary>
+        public virtual void OnCreate() { }
+
+        /// <summary>
+        /// /G:变为可用状态时候调用
+        /// </summary>
+        public abstract void OnEnable();
+
+        /// <summary>
+        /// G:变为不可用状态时候调用
+        /// </summary>
+        public abstract void OnDisable();
+
+
+
+    }
+}
